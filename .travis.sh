badcd6403647176fec5ec67af1e53605dc699239
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2021-09-30 17:44:17 +0900

    fix conflict on .travis.sh

--- .travis.sh
+++ .travis.sh
@@ -46,13 +46,7 @@ fi
 # set DEBIAN_FRONTEND=noninteractive
 echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
 sudo sh -c "echo \"deb ${DEB_REPOSITORY} `lsb_release -cs` main\" > /etc/apt/sources.list.d/ros-latest.list"
-if [ "$CI_ROS_DISTRO" == "melodic" ]; then
-    sudo DEBIAN_FRONTEND=noninteractive apt-get install -y -qq tzdata
-    sudo apt-get install -qq -y gnupg2
-    sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
-else
-    wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
-fi
+wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
 sudo apt-get update -qq
 if [[ "$CI_ROS_DISTRO" ==  "noetic" ]]; then
     sudo apt-get install -qq -y python3-rosdep python3-catkin-tools
@@ -81,10 +75,8 @@ catkin build --limit-status-rate 0.1 --no-notify -DCMAKE_BUILD_TYPE=Release
   # Build tests
 catkin build --limit-status-rate 0.1 --no-notify --make-args tests
   # Run tests
-####### kinteic has problem on runnning gazebo with image on headless mode????
-# if [ "$CI_ROS_DISTRO" == "kinetic" ]; then
+####### avoid problem on runnning gazebo with image on headless mode????
   touch src/rtmros_nextage/nextage_calibration/CATKIN_IGNORE
-# fi
 #######
 catkin run_tests -j1 -p1
   # check test (this only works from indigo onward)
